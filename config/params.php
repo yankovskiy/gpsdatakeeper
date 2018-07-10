cb26de6b306fd2f0e1ccdaecdb9b7217b82018f5
Artem Yankovskiy <artemyankovskiy@gmail.com>
2018-07-10 17:15:23 +1000

    v0.1.4
    Added feature for upload GPX and KML
    Memory optimisation
    Code refactoring

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'robotEmail' => 'robot@gpsdatakeeper.com',
     'supportEmail' => 'support@gpsdatakeeper.com',
     'user.passwordResetTokenExpire' => 3600,
-    'version' => '0.1.3',
+    'version' => '0.1.4',
 ];
